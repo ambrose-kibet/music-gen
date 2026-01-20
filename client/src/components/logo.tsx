0c793b1c3c693a842f3eac58dbe47476cea4ee69
ambrose-kibet <ambrosekibet36@gmail.com>
2026-01-20 15:47:28 +0300

    feat(components): add FormError, FormSuccess, and Logo components

---
+++ client/src/components/logo.tsx
@@ -0,0 +1,10 @@
+const Logo: React.FC = () => {
+  return (
+    <div className="flex items-center gap-x-2">
+      <span className="text-lg  text-primary font-bold sm:text-xl">
+        Music Gen
+      </span>
+    </div>
+  );
+};
+export default Logo;
